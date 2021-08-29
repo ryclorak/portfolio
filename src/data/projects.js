c30790792943b4f5b85aabb9f4b8ee49b4ad41dd
ryclorak <ryclorak@gmail.com>
2021-08-29 14:02:10 -0700

    fix

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,3 @@
-import project1 from '../assets/project1.png';
-import project2 from '../assets/project2.png';
-import project3 from '../assets/project3.png';
 import droneSim from '../assets/droneSim.jpg'
 import spiderBot from '../assets/spiderRobot.jpg'
 import jShelfV4 from '../assets/jShelfV4.PNG'
