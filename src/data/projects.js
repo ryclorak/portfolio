9b4a107b3167382b15848c1478e0dae538c4e447
ryclorak <ryclorak@gmail.com>
2021-08-29 13:56:36 -0700

    projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,31 @@
 import project1 from '../assets/project1.png';
 import project2 from '../assets/project2.png';
 import project3 from '../assets/project3.png';
+import droneSim from '../assets/DroneSimulatorHoops.png'
+import spiderBot from '../assets/spiderRobot.jpg'
+import jShelfV4 from '../assets/jShelfV4.png'
 
 const PROJECTS = [
   {
     id: 1,
-    title: 'Example React Lication',
-    description: 'Reaction, Jayess alalala toilet toilet toilet lala lala',
-    link: 'https://github.com/15Dkatz/example',
-    image: project1
+    title: 'Drone Simulator',
+    description: 'Made using Unity3d, for a drone piloting course at ARC',
+    link: 'https://www.faa.gov/uas/commercial_operators/',
+    image: droneSim
   },
   {
     id: 2,
-    title: 'My API',
-    description: 'REST API built from scratch with GET and POST requests',
-    link: 'https://github.com/15Dkatz/example',
-    image: project2
+    title: 'Embedded Systems',
+    description: 'I\'ve made numerous projects using Arduino and Raspberry Pi',
+    link: 'https://github.com/ryclorak/Arduino',
+    image: spiderBot
   },
   {
     id: 3,
-    title: 'Final Operating Coprse',
-    description: 'Unique final project for university Operating Systems corpse.',
-    link: 'https://github.com/15Dkatz/example',
-    image: project3
+    title: '3D Designs/Prints',
+    description: 'I enjoy 3d designing and printing! Check out my thingiverse.',
+    link: 'https://www.thingiverse.com/ryclorak/designs',
+    image: jShelfV4
   }
 ];
 
