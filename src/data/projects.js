5f340029a94cf241231a1c8858d368ee84309e7e
ryclorak <ryclorak@gmail.com>
2021-08-29 18:07:56 -0700

    fix

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,14 +6,14 @@ const PROJECTS = [
   {
     id: 1,
     title: 'Programming',
-    description: 'Made Drone simulator with Unity3D for a drone piloting course at ARC',
+    description: 'Drone simulator made in Unity3D for a drone piloting course.',
     link: 'https://www.faa.gov/uas/commercial_operators/',
     image: droneSim
   },
   {
     id: 2,
     title: 'Embedded Systems',
-    description: 'I\'ve made numerous projects using Arduino and Raspberry Pi',
+    description: 'I\'ve made numerous projects using Arduino and Raspberry Pi.',
     link: 'https://github.com/ryclorak/Arduino',
     image: spiderBot
   },
