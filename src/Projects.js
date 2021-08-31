d73fb65f649e779871bdb9505e1ea0f47187ba0d
ryclorak <ryclorak@gmail.com>
2021-08-31 07:21:03 -0700

    added linkText

--- src/Projects.js
+++ src/Projects.js
@@ -12,7 +12,7 @@ class Project extends Component {
       <h3>{title}</h3>
       <img src={image} alt='profile' style={{ width: 200, height: 120 }} />
       <p>{description}</p>
-      <a href={link}>{link}</a>
+      <a href={link}>{linkText}</a>
       </div>
     )
   }
